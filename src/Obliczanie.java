83843d9ce7faa21b991475d391bdc3401cea5600
camperik94 <tomaszjach94@gmail.com>
2018-01-02 09:46:34 +0100

    add count int

--- src/Obliczanie.java
+++ src/Obliczanie.java
@@ -1,4 +1,30 @@
 
 public class Obliczanie {
-	
+	private int liczba;
+
+	public int getLiczba() {
+		return liczba;
+	}
+
+	public void setLiczba(int liczba) {
+		this.liczba = liczba;
+	}
+
+	public Obliczanie(int liczba) {
+		this.liczba = liczba;
+	}
+
+	public int funkcja() {
+		int zwrocona = liczba;
+		int n;
+		int count = 0;
+
+		while (zwrocona > 0) {
+			System.out.println("Zwrocona = " + zwrocona);
+			count++;
+			zwrocona = zwrocona / 10;
+			System.out.println("Count = " + count);
+		}
+		return count;
+	}
 }
--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Podaj liczbe calkowita");
+		
+		int liczba = in.nextInt();
+		
+		Obliczanie obl = new Obliczanie(liczba);
+		
+		System.out.println(obl.funkcja());
 		
 	}
 }
